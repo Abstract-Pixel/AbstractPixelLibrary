1b7c95b2d73a04e2769743e18d0c096fa255d39b
Avinash Prabhu <avi.g751@gmail.com>
2026-02-16 17:33:12 +0400

    For now polymorphic attribute is functional

--- Assets/TestScripts/PolymophicTesting/Test.cs
+++ Assets/TestScripts/PolymophicTesting/Test.cs
@@ -1,8 +1,8 @@
 using System;
 using UnityEngine;
 
-[Serializable]
-public abstract class Test
+
+public interface  Test
 {
 
 }
--- Assets/TestScripts/PolymophicTesting/TestManager.cs
+++ Assets/TestScripts/PolymophicTesting/TestManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using AbstractPixel.Utility;
 
 
 public class TestManager : MonoBehaviour
